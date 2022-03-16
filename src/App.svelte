7722e1b44c2ac0a3fef23a78220efe03773e1bb9
Vinicius-SO <vhisoares39@gmail.com>
2022-03-16 16:19:58 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import HeaderContainer from './components/HeaderContainer.svelte';
+</script>
+
+<main>
+  <HeaderContainer/>
+</main>
+
+
+<style>
+
+  :global(html){
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --blue: #5429CC;
+        --green: #33cc95;
+
+        --blue-light: #6933ff;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --background: #f0f2f5;
+        --shape: #FFFFFF;
+
+  }
+    
+  :global(*){
+      margin:0;
+      padding:0;
+      box-sizing:border-box;
+  }
+
+  main{
+      background: var(--background);
+      width: 100vw;
+      height: 100vh;
+      
+  }
+    
+
+  main{
+    background: --;
+  }
+</style>
---
+++ src/components/HeaderContainer.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from './Header.svelte'
+</script>
+
+<header>
+    <Header/>
+</header>
+
+<style>
+    
+    header{
+        margin: 0 auto;
+        width: 100vw;
+        height: 30%;
+        background: var(--blue)
+    }
+    
+</style>
